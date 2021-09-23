97ca536df70668adf484cb7a49067d83baded84f
Chris Risden <christopherrisden@gmail.com>
2021-09-22 19:51:55 -0700

    seem to be creating a grid

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //on button click let's create a board of a certain size
 
-const createBoard = document.querySelector('#createBoard')
+const container = document.querySelector('#container')
 const grid = document.querySelector('#grid')
 const pixel = document.createElement('div');
 pixel.innerHTML = 'hello'
@@ -8,15 +8,39 @@ pixel.innerHTML = 'hello'
 const populateGrid = function (e) {
   const input = prompt('enter a number 1-64')
 
-  for(let i = 0; i < input; i++) {
-    for(let j = 0; j < input; j++) {
-      grid.appendChild(pixel)
-    }
-    grid.appendChild(pixel)
-  }
+  makeRows(input);
+  makeCells(input);
+
 
 }
 
+const makeRows = function (num) {
+  //add number of rows
+  for (let i = 0; i < num; i++) {
+    let row = document.createElement('div')
+    row.innerHTML ='hello'
+    row.id = 'row' + i;
+    row.className = 'row'
+    container.appendChild(row);
+
+  }
+}
+
+const makeCells = function (num) {
+  for (let i = 0; i < num; i++) {
+
+    let row = 'row' + i;
+    row = document.getElementById(row)
+    for (let j = 0; j<num; j++) {
+      let cell = document.createElement('div');
+      cell.innerHTML = 'world'
+      cell.className = 'cell'
+      row.appendChild(cell)
+    }
+  }
+
+  //add number of cells in row
+}
 
 
 
